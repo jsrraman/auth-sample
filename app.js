7d8100c52b1889e67ded1af88f98af343894b691
Rajaraman <rajaraman.ks@gmail.com>
2015-12-08 16:50:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+    app = express();
+
+const router = require('./routes/router');
+
+// Setup routing
+app.use('/', router);
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
